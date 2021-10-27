e2d5551c0899070ad78e44d9056519d084e8a00f
Soobin, Jo <90607293+jo-soobin@users.noreply.github.com>
2021-10-27 21:11:38 +0900

    Add files via upload

---
+++ 4-3_튜플.py
@@ -0,0 +1,18 @@
+menu= ("돈까스", "치즈까스")
+print(menu[0])
+print(menu[1])
+
+#튜플은 add의 값을 제공하지 않음
+#menu.add("생선까스")
+
+name = "종국"
+age = 20
+hobby = "coding"
+print(name, age, hobby)
+
+#튜플 이용하면
+name, age, hobby =( "종국", 20 ,"coding" )
+print(name, age, hobby)
+
+
+
